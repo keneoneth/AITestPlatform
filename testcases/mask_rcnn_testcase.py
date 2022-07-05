3be60865b41dcbace985c68c3734d6d953eb8570
keneoneth <keneoneth18@gmail.com>
2022-07-05 22:58:20 +0800

    removed metric during model compile

--- testcases/mask_rcnn_testcase.py
+++ testcases/mask_rcnn_testcase.py
@@ -1,5 +1,4 @@
-import os
-from pickletools import optimize
+import copy
 import time
 import tensorflow as tf
 import numpy as np
@@ -7,8 +6,6 @@ try:
     import Image
 except ImportError:
     from PIL import Image
-
-import copy
 
 # {b'num_cases_per_batch': 10000, b'label_names': [b'airplane', b'automobile', b'bird', b'cat', b'deer', b'dog', b'frog', b'horse', b'ship', b'truck'], b'num_vis': 3072}
 
@@ -18,14 +15,6 @@ def save_img(arr,fname):
     arr = np.array(arr,dtype=np.uint8)
     im = Image.fromarray(arr)
     im.save(fname+".jpeg")
-
-def get_my_metric(metric_name):
-    def fn(y_true, y_pred):
-        # custom_loss = tf.reduce_mean(y_pred, keepdims=True)
-        return y_pred
-
-    fn.__name__ = 'metricname_{}'.format(metric_name)
-    return fn
 
 def mytest(**args):
     
@@ -51,12 +40,11 @@ def mytest(**args):
         lr=testconfig['learning_rate'], momentum=testconfig['momentum'],
         clipnorm=testconfig['gradient_clip_norm'])
 
-    # TODO: add model weight
     # Add Losses
     loss_names = [ "rpn_class_loss", "rpn_bbox_loss", "mrcnn_class_loss", "mrcnn_bbox_loss", "mrcnn_mask_loss"]
     for name in loss_names:
         layer = model.get_layer(name)
-        loss = tf.reduce_mean(layer.output, keepdims=True) #* testconfig["LOSS_WEIGHTS"].get(name, 1.)
+        loss = tf.reduce_mean(layer.output, keepdims=True) * testconfig["LOSS_WEIGHTS"].get(name, 1.)
         model.add_loss(loss)
     
     # Add L2 Regularization
@@ -67,24 +55,10 @@ def mytest(**args):
     #     if 'gamma' not in w.name and 'beta' not in w.name]
     # model.add_loss(lambda : tf.add_n(reg_losses))
 
-    print("losses",model.losses)
+    print("[info] losses",model.losses)
 
     # compile model
     model.compile(optimizer=optimizer,loss=None,metrics=None)
-
-    #TODO: add metrics
-    # Add metrics for losses
-    # metrics = [get_my_metric("custom_metric")]
-    model.metrics_tensors = []
-    for name in loss_names:
-        if name in model.metrics_names:
-            continue
-        layer = model.get_layer(name)
-        model.metrics_names.append(name)
-        loss = tf.reduce_mean(layer.output, keepdims=True) #* testconfig["LOSS_WEIGHTS"].get(name, 1.)
-        # add_metrics?
-        model.metrics_tensors.append(lambda : loss)
-    print("metrics_tensors",model.metrics_names)
 
     print(model.summary())
     
@@ -95,8 +69,8 @@ def mytest(**args):
     # start training model fit
     if opt_set.opt_train:
         # Data generators
-        train_generator = data.get_train_data_generator(copy.deepcopy(testconfig),testconfig['batch_size'],copy.deepcopy(anchors))
-        valid_generator = data.get_valid_data_generator(copy.deepcopy(testconfig),testconfig['batch_size'],copy.deepcopy(anchors))
+        train_generator = data.get_train_data_generator(copy.deepcopy(testconfig),testconfig['batch_size'],copy.deepcopy(anchors),output_len)
+        valid_generator = data.get_valid_data_generator(copy.deepcopy(testconfig),testconfig['batch_size'],copy.deepcopy(anchors),output_len)
 
         for epoch_index in range(testconfig['epochs']):
             print("entering epoch %d ..." % epoch_index)
@@ -115,7 +89,7 @@ def mytest(**args):
     if opt_set.opt_test:
         config = model.get_config() # Returns pretty much every information about your model
 
-        test_generator = data.get_test_data_generator(copy.deepcopy(testconfig),testconfig['batch_size'],copy.deepcopy(anchors))
+        test_generator = data.get_test_data_generator(copy.deepcopy(testconfig),testconfig['batch_size'],copy.deepcopy(anchors),output_len)
         test_ret = model.evaluate(x=test_generator, verbose=0)
 
         test_duration = time.time() - test_start_time
