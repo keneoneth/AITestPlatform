ccb82e7ddab01d20bd695dfbd7b1048b49643e28
Li Chun Ting <keneo@DESKTOP-0UPGAMU.localdomain>
2023-02-14 00:49:23 +0800

    Decorate run_aitest.py

--- run_aitest.py
+++ run_aitest.py
@@ -1,25 +1,43 @@
-from ast import arg
+#!/usr/bin/env python
+
+
+"""
+run_aitest.py/aitest - aitest driver command-line interface
+"""
+
 import os
 import toml
 import argparse
 import json
 
-
-PATH_TO_TESTRUN = "./testrun/"
-PATH_TO_TESTCASE = "./testcases/"
-PATH_TO_MODEL = "./models/"
+# path to store the results of test run
 PATH_TO_RESULT = "./results/"
-PATH_TO_DATASET = "./datasets/"
 
-KEY_TITLE = "title"
-KEY_DATASETS = "datasets"
-KEY_MODELS = "models"
-KEY_TESTCASES = "testcases"
-KEY_TESTRUN = "testrun"
+# stores the compulsory keys needed in test run toml
+class TomlKeys:
 
+    KEY_TITLE = "title"
+    KEY_DATASETS = "datasets"
+    KEY_MODELS = "models"
+    KEY_TESTCASES = "testcases"
+    KEY_TESTRUN = "testrun"
+
+    KEY_DATASET = "dataset"
+    KEY_MODEL = "model"
+    KEY_TESTCASE = "testcase"
+    KEY_OUTFORMAT = "out_format"
+
+    def validate_toml(loaded_toml):
+        assert TomlKeys.KEY_TITLE in loaded_toml
+        assert TomlKeys.KEY_DATASETS in loaded_toml
+        assert TomlKeys.KEY_MODELS in loaded_toml
+        assert TomlKeys.KEY_TESTCASES in loaded_toml
+        assert TomlKeys.KEY_TESTRUN in loaded_toml
+
+# stores the custom options used during test run
 class OptionSet:
 
-    def __init__(self,opt_train,opt_test,opt_model_path):
+    def __init__(self, opt_train, opt_test, opt_model_path):
 
         self.opt_train = opt_train
         self.opt_test = opt_test
@@ -33,10 +51,10 @@ class OptionSet:
             assert self.opt_model_path != None, "[error] please specify model path for testing if train==False"
 
     def __str__(self) -> str:
-        return "opt_train:{}|opt_test:{}|opt_model_path:{}|opt_save_per_epoch:{}".format(self.opt_train,self.opt_test,self.opt_model_path,self.opt_save_per_epoch)
+        return "opt_train:{}|opt_test:{}|opt_model_path:{}|opt_save_per_epoch:{}".format(self.opt_train, self.opt_test, self.opt_model_path, self.opt_save_per_epoch)
 
     @staticmethod
-    def get_saveweight_cb(result_path,save_best_only=True,monitor='loss'):
+    def get_saveweight_cb(result_path, save_best_only=True, monitor='loss'):
         import tensorflow as tf
         return tf.keras.callbacks.ModelCheckpoint(
             filepath=result_path,
@@ -48,51 +66,51 @@ class OptionSet:
             period=1
         )
 
-class TestRun:
 
-    KEY_DATASET = "dataset"
-    KEY_MODEL = "model"
-    KEY_TESTCASE = "testcase"
-    KEY_OUTFORMAT = "out_format"
+class TestRun:
 
     title = ""
     LOADED_DATASETS = {}
     LOADED_MODELS = {}
     LOADED_TESTCASES = {}
-    
+
     @staticmethod
-    def load_objects(datasets,models,testcases):
+    def load_objects(datasets, models, testcases):
         TestRun.LOADED_DATASETS = datasets
         TestRun.LOADED_MODELS = models
         TestRun.LOADED_TESTCASES = testcases
-    
+
     @staticmethod
     def set_title(title):
         TestRun.title = "".join(title.split())
 
-    def __init__(self,testindex,dataset_key,model_key,testcase_key,out_format):
+    def __init__(self, testindex, dataset_key, model_key, testcase_key, out_format):
         self.testindex = testindex
         self.dataset_key = dataset_key
         self.model_key = model_key
         self.testcase_key = testcase_key
         self.out_format = out_format
-    
-    def __str__(self):
-        return "dataset:{}|model:{}|testcase:{}".format(self.dataset_key,self.model_key,self.testcase_key)
-    
-    def out_json_name(self):
-        return '%s/%s_%s.json'%(self.title,self.testcase_key,str(self.testindex))
 
-    def run_test(self,opt_set):
+    def __str__(self):
+        return "dataset:{}|model:{}|testcase:{}".format(self.dataset_key, self.model_key, self.testcase_key)
+
+    def out_json_name(self):
+        return '%s/%s_%s.json' % (self.title, self.testcase_key, str(self.testindex))
+
+    def run_test(self, opt_set):
         from _scripts.load_dataset import DataLoad
-        data = DataLoad.load_dataset(self.dataset_key,TestRun.LOADED_DATASETS[self.dataset_key])
+        data = DataLoad.load_dataset(
+            self.dataset_key, TestRun.LOADED_DATASETS[self.dataset_key])
         from _scripts.load_model import ModelLoad
-        model = ModelLoad.load_model(self.model_key,TestRun.LOADED_MODELS[self.model_key])
+        model = ModelLoad.load_model(
+            self.model_key, TestRun.LOADED_MODELS[self.model_key])
         from _scripts.load_testcase import TestcaseLoad
-        testfunc = TestcaseLoad.load_testcase(self.testcase_key,TestRun.LOADED_TESTCASES[self.testcase_key])
-        rets = testfunc(data=data,model_key=self.model_key,model=model,testfunc=testfunc,testconfig=TestRun.LOADED_TESTCASES[self.testcase_key],result_path=PATH_TO_RESULT+'/'+self.title+"/",opt_set=opt_set)
-        assert isinstance(rets,list)
-        for index,ret in enumerate(rets):
+        testfunc = TestcaseLoad.load_testcase(
+            self.testcase_key, TestRun.LOADED_TESTCASES[self.testcase_key])
+        rets = testfunc(data=data, model_key=self.model_key, model=model, testfunc=testfunc,
+                        testconfig=TestRun.LOADED_TESTCASES[self.testcase_key], result_path=PATH_TO_RESULT+'/'+self.title+"/", opt_set=opt_set)
+        assert isinstance(rets, list)
+        for index, ret in enumerate(rets):
             if not index < len(self.out_format):
                 print("[warning] results is more than out format length")
                 continue
@@ -101,41 +119,53 @@ class TestRun:
                     json.dump(ret, f)
                     f.close()
             else:
-                print("[warning] out format {} not supported yet".format(self.out_format[index]))
-        
-def validate_toml(loaded_toml):
-    assert KEY_TITLE in loaded_toml
-    assert KEY_DATASETS in loaded_toml
-    assert KEY_MODELS in loaded_toml
-    assert KEY_TESTCASES in loaded_toml
-    assert KEY_TESTRUN in loaded_toml
+                print("[warning] out format {} not supported yet".format(
+                    self.out_format[index]))
 
-def run_toml(tomlfile,opt_set):
+
+
+
+
+def run_toml(tomlfile, opt_set):
     # print(opt_train,opt_test,opt_model_path,opt_save_per_epoch)
     with open(tomlfile) as f:
+
         loaded_toml = toml.load(f)
-        validate_toml(loaded_toml)
-        print("[info] processing {} with title {}".format(f.name,loaded_toml[KEY_TITLE]))
-        TestRun.set_title(loaded_toml[KEY_TITLE])
+        TomlKeys.validate_toml(loaded_toml)
+        print("[info] processing {} with title {}".format(
+            f.name, loaded_toml[TomlKeys.KEY_TITLE]))
+
+        TestRun.set_title(loaded_toml[TomlKeys.KEY_TITLE])
         if not os.path.exists(PATH_TO_RESULT+TestRun.title):
             os.mkdir(PATH_TO_RESULT+TestRun.title)
-        TestRun.load_objects(loaded_toml[KEY_DATASETS],loaded_toml[KEY_MODELS],loaded_toml[KEY_TESTCASES])
-        for testindex,testrun_dict in enumerate(loaded_toml[KEY_TESTRUN]):
-            testrun = TestRun(testindex,testrun_dict[TestRun.KEY_DATASET],testrun_dict[TestRun.KEY_MODEL],testrun_dict[TestRun.KEY_TESTCASE],testrun_dict[TestRun.KEY_OUTFORMAT])
+        TestRun.load_objects(loaded_toml[TomlKeys.KEY_DATASETS],
+                             loaded_toml[TomlKeys.KEY_MODELS], loaded_toml[TomlKeys.KEY_TESTCASES])
+                             
+        for testindex, testrun_dict in enumerate(loaded_toml[TomlKeys.KEY_TESTRUN]):
+            testrun = TestRun(testindex, testrun_dict[TomlKeys.KEY_DATASET], testrun_dict[TomlKeys.KEY_MODEL],
+                              testrun_dict[TomlKeys.KEY_TESTCASE], testrun_dict[TomlKeys.KEY_OUTFORMAT])
             print("[info] running {}".format(testrun))
             testrun.run_test(opt_set)
-        print("[info] testrun {} with title {} done !!!".format(f.name,loaded_toml[KEY_TITLE]))
+        print("[info] testrun {} with title {} done !!!".format(
+            f.name, loaded_toml[TomlKeys.KEY_TITLE]))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t","--toml",required=True,help="toml files of testcase e.g. abc.toml,def.toml,ghl.toml")
-    parser.add_argument("--train",required=False,default=None,help="set the testcase to only train the model",action='store_true')
-    parser.add_argument("--test",required=False,default=None,help="set the testcase to only to test the specified model path",action='store_true')
-    parser.add_argument("-m","--model_path",required=False,default=None,help="specify the model path for training/testing (compulsory for testing if train==False)")
+    parser.add_argument("-t", "--toml", required=True,
+                        help="toml files of testcase separated by ',' e.g. abc.toml,def.toml,ghl.toml")
+    parser.add_argument("--train", required=False, default=None,
+                        help="set the testcase to only train the model", action='store_true')
+    parser.add_argument("--test", required=False, default=None,
+                        help="set the testcase to only to test the specified model path", action='store_true')
+    parser.add_argument("-m", "--model_path", required=False, default=None,
+                        help="specify the model path for training/testing (compulsory for testing if train==False)")
     args = parser.parse_args()
     tomls_to_run = args.toml.split(",")
-    
-    opt_set = OptionSet(args.train,args.test,args.model_path)
 
+    # set up option set
+    opt_set = OptionSet(args.train, args.test, args.model_path)
+
+    # run toml files one by one
     for tomlfile in tomls_to_run:
-        run_toml(tomlfile,opt_set)
+        run_toml(tomlfile, opt_set)
